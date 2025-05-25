7b5d20e03ac77c5a7408a8341d3b2adfbb64e9eb
mariem mathlouthi <mariemmathlouthi15@gmail.com>
2025-05-25 16:19:34 +0100

    update design

--- front-end/src/components/Admin/SidebarMenu.vue
+++ front-end/src/components/Admin/SidebarMenu.vue
@@ -4,7 +4,7 @@
 	  <!-- Sidebar content goes here -->
 	<section id="sidebar">
 		  <a href="#" class="brand">
-			  <i class='bx bxs-smile'></i>
+			  <i class='bx bxs-cog'></i>
 			  <span class="text">Admin</span>
 		  </a>
 		  <ul class="side-menu top">
@@ -16,77 +16,73 @@
 			  </li>
 			  <li>
 				  <a href="#">
-					  <i class='bx bxs-shopping-bag-alt'></i>
-					  <router-link to="/TuteursListAdmin"><span class="text">Les Tuteur</span></router-link>
+					  <i class='bx bxs-user-badge'></i>
+					  <router-link to="/TuteursListAdmin"><span class="text">Les Tuteurs</span></router-link>
 				  </a>
 			  </li>
 			  <li>
 				  <a href="#">
-					  <i class='bx bxs-shopping-bag-alt'></i>
+					  <i class='bx bxs-file'></i>
 					  <router-link to="/TuteursRequests"><span class="text">Demande Tuteurs</span></router-link>
 				  </a>
 			  </li>
 			  <li>
 				  <a href="#">
-					  <i class='bx bxs-shopping-bag-alt'></i>
+					  <i class='bx bxs-category'></i>
 					  <router-link to="/CategoryList"><span class="text">Category</span></router-link>
 				  </a>
 			  </li>
 			  <li>
 				  <a href="#">
-					  <i class='bx bxs-shopping-bag-alt'></i>
-					  <router-link to="/SpecialiteList"><span class="text">specialite</span></router-link>
+					  <i class='bx bxs-certification'></i>
+					  <router-link to="/SpecialiteList"><span class="text">Specialité</span></router-link>
 				  </a>
 			  </li>
+			  <!-- Modifié pour l'icône Domaine -->
+			<li>
+			<a href="#">
+				<i class='bx bxs-layer'></i>
+				<router-link to="/DomaineList"><span class="text">Domaine</span></router-link>
+			</a>
+			</li>
 			  <li>
 				  <a href="#">
-					  <i class='bx bxs-shopping-bag-alt'></i>
-					  <router-link to="/DomaineList"><span class="text">Domaine</span></router-link>
-				  </a>
-			  </li>
-			  <li>
-				  <a href="#">
-					  <i class='bx bxs-shopping-bag-alt'></i>
+					  <i class='bx bxs-graduation'></i>
 					  <router-link to="/NiveauList"><span class="text">Niveau</span></router-link>
 				  </a>
 			  </li>
 			  <li>
 				  <a href="#">
-					  <i class='bx bxs-shopping-bag-alt'></i>
+					  <i class='bx bxs-factory'></i>
 					  <router-link to="/SecteurList"><span class="text">Secteur</span></router-link>
 				  </a>
 			  </li>
-
-			  
 			  <li>
 				  <a href="#">
-					  <i class='bx bxs-shopping-bag-alt'></i>
+					  <i class='bx bxs-briefcase-alt'></i>
 					  <router-link to="/OffresListAdmin"><span class="text">Les offres</span></router-link>
 				  </a>
 			  </li>
-  
-			  
-			  <li>
-	  <a href="#">
-		  <i class='bx bxs-building'></i> <!-- Changer la classe d'icône pour une icône d'entreprise -->
-		  <router-link to="/UsersList"><span class="text">Les entreprises</span></router-link>
-	  </a>
-  </li>
 			  <li>
 				  <a href="#">
-					  <i class='bx bxs-group'></i>
-					  <router-link to="/UsersListEtudiants"><span class="text">Les etudiants</span></router-link>
+					  <i class='bx bxs-building'></i>
+					  <router-link to="/UsersList"><span class="text">Les entreprises</span></router-link>
+				  </a>
+			  </li>
+			  <li>
+				  <a href="#">
+					  <i class='bx bxs-user'></i>
+					  <router-link to="/UsersListEtudiants"><span class="text">Les étudiants</span></router-link>
 				  </a>
 			  </li>
 			  <li :class="{ active: $route.path === '/AdminFeedbacks' }">
-			<a href="#">
-			  <i class='bx bxs-message-alt-detail'></i>
-			  <router-link to="/AdminFeedbacks"><span class="text">Feedbacks</span></router-link>
-			</a>
-		  </li>
+				<a href="#">
+				  <i class='bx bxs-message-alt-detail'></i>
+				  <router-link to="/AdminFeedbacks"><span class="text">Feedbacks</span></router-link>
+				</a>
+			  </li>
 		  </ul>
 		  <ul class="side-menu">
-			  
 			  <li>
 				  <a href="#" class="logout">
 					  <i class='bx bxs-log-out-circle'></i>
@@ -96,14 +92,13 @@
 		  </ul>
 	  </section>
 	</aside>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
 	// SidebarMenu component logic
-  }
-  </script>
-  
-  <style scoped>
-  </style>
-  
+}
+</script>
+
+<style scoped>
+</style>
